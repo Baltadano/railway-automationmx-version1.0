80cbeeb4bdb6963c020c5a454661e8f24c9d50f1
Mariano BJL <marianobjl@MacBook-Air-de-Mariano.local>
2026-02-02 20:30:10 -0600

    onceaba commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import SectionBeneficios from './componentes/BeneficiosSection';
 import ServiciosSection from './componentes/ServiciosSection';
 import ProcesoSection from './componentes/ProcesoSection';
 import RedesSocialesSection from './componentes/RedesSocialesSection';
-import Section from './componentes/Section';
-import Section2 from './componentes/Section2';
 import ContactForm from './componentes/ContactForm';
 import CasosExitoSection from './componentes/CasosExitoSection';
 import Footer from './componentes/Footer';
-import logo from './logo.png';
 import './Styles.css';
-import SimpleChat from './componentes/SimpleChat';
-import ChatWidgetTyping from './componentes/ChatWidgetTyping';
 
 function App() {
   return (
@@ -32,12 +27,10 @@ function App() {
       <div className="absolute inset-0 z-0" />
       <Header />
       <main className="relative z-10">
-        {/* <Section id="inicio" title="Nuestros servicios " children="Atiende, guiando y agendando a tu cliente hacia tu servicio de forma inteligente como tu lo harias personalmente ..." />  */}
-        <SectionBeneficios id="beneficios"></SectionBeneficios>
+      <SectionBeneficios id="beneficios"></SectionBeneficios>
         <ServiciosSection id ="servicios"></ServiciosSection>
         <ProcesoSection id="proceso"></ProcesoSection>
         <CasosExitoSection id="casos-exito" ></CasosExitoSection>
-        
         <ContactForm id="contacto"/>
          <RedesSocialesSection ></RedesSocialesSection>
       </main>
